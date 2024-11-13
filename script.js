0868e6f642c7cff6459800855352a03389c28ff8
German <hondusite@gmail.com>
2024-11-12 21:05:00 -0600

    app

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(reg => console.log('Service Worker registrado', reg))
+        .catch(err => console.log('Error al registrar el Service Worker', err));
+}
 const rutinaPorDia = {
     Lunes: {
         ejercicios: [
